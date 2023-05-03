e79712509392c3c443277d12a3535a3edac4a375
timDeHof <ttdehof@gmail.com>
2023-05-03 11:51:41 -0400

    Time: 56 ms (85.53%) | Memory: 41.8 MB (75.81%) - LeetSync

--- count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.js
+++ count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.js
@@ -4,11 +4,8 @@
  * @return {number}
  */
 var countOdds = function(low, high) {
-    let count = 0
-    for( let i = low; i <= high; i++){
-        if(i % 2 !== 0){
-            count++
-        }
-    }
-    return count
+   const isLowOdd = low % 2 === 1;
+   const isHighOdd = high % 2 === 1;
+   const count = Math.floor((high - low + isLowOdd + isHighOdd) / 2)
+   return count 
 };
