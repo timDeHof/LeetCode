f06bfa308d6cc96266fbadbe24d8e5de8ff021a2
timDeHof <ttdehof@gmail.com>
2023-05-08 16:22:13 -0400

    Time: 48 ms (97.11%) | Memory: 42.1 MB (70.41%) - LeetSync

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let res =[]
+
+    for (let i = 0; i < numRows; i++){
+        let row = new Array(i + 1).fill(1)
+        for (let j = 1; j < i; j++){
+            row[j] = res[i - 1][j - 1] + res[i - 1][j]
+        }
+        res.push(row)
+    }
+    return res
+};
